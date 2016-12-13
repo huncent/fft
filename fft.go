bfffa664fa5d7a1c4297f2b28d4d1190bbe3ca6e
pgrew <mike@blacklabeldata.io>
2016-12-13 13:38:01 -0800

    DFFT function is complete and main runs an example.

--- fft.go
+++ fft.go
@@ -1,29 +1,54 @@
 package main
 
-import "math"
+import (
+	"fmt"
+	"math"
+	"math/cmplx"
+)
 
 func ZeroPadArray(x []float64, padToSize int) []float64 {
-	if length(x) <= padToSize {
+	if len(x) >= padToSize {
 		return x
 	}
-	output = make(float64, padToSize)
-	for i := 0; i < length(x); i++ {
+	output := make([]float64, padToSize)
+	for i := 0; i < len(x); i++ {
 		output[i] = x[i]
 	}
-	for j := length(x); j < padToSize; j++ {
+	for j := len(x); j < padToSize; j++ {
 		output[j] = 0.0
 	}
 	return output
 }
 
-func NextPowerOfTwo(x int64) {
-	next = math.Pow(2, math.Ceil(math.Log2(x)/math.Log2(2)))
+// func NextPowerOfTwo(x int64) {
+// 	next := math.Pow(2, math.Ceil(math.Log2(x)/math.Log2(2)))
+// 	return
+// }
+
+func DFFT(x []float64, y []complex128, n, s int) {
+	if n == 1 {
+		y[0] = complex(x[0], 0)
+		return
+	}
+	DFFT(x, y, n/2, 2*s)
+	DFFT(x[s:], y[n/2:], n/2, 2*s)
+	for i := 0; i < n/2; i++ {
+		theta := -2.0 * math.Pi * float64(i) / float64(n)
+		term := cmplx.Rect(1.0, theta) * y[i+n/2]
+		temp := y[i]
+		y[i] = temp + term
+		y[i+n/2] = temp - term
+	}
+	return
 }
 
-func DFFT(x []float64) {
-	// if len(x) != power of 2 then pad
-	if length(x)%2 == 0 {
-		x = ZeroPadArray(x)
+func main() {
+	x := []float64{math.Pi / 4, math.Pi / 2, math.Pi * 3 / 4, math.Pi}
+	x = ZeroPadArray(x, 32)
+	fmt.Println(x)
+	y := make([]complex128, len(x))
+	DFFT(x, y, len(x), 1)
+	for _, val := range y {
+		fmt.Println(val)
 	}
-	// TODO: the rest
 }
